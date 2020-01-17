dd5f43b7daa31ce69bc47680d212fcd1f652c32c
tylerpragides <32971540+tylerpragides@users.noreply.github.com>
2020-01-17 11:36:05 -0800

    k

--- 1-Basic-HTML/sketch.js
+++ 1-Basic-HTML/sketch.js
@@ -1,7 +1,20 @@
 function setup() {
   // put setup code here
+  let elements = document.getElementsByClassName("list1");
+
+
+  for (let elem of elements) {
+    elem.addEventListener("click",myFunction);
+  }
+
+
+  function myFunction(event) {
+    event.target.innerHTML = "";
+    // document.removeChild(event.target);
+  }
 }
 
 function draw() {
   // put drawing code here
+
 }
