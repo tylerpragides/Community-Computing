9d68d5a75ea35272840637e5e59b80d39bf45c4f
tylerpragides <32971540+tylerpragides@users.noreply.github.com>
2020-01-22 09:39:15 -0800

    k

--- 1-Basic-HTML/sketch.js
+++ 1-Basic-HTML/sketch.js
@@ -1,20 +1,27 @@
 function setup() {
   // put setup code here
   let elements = document.getElementsByClassName("list1");
-
+  document.getElementById('myHeader').addEventListener("click",changeFontSize);
 
   for (let elem of elements) {
     elem.addEventListener("click",myFunction);
   }
 
-
   function myFunction(event) {
     event.target.innerHTML = "";
     // document.removeChild(event.target);
+  }
+
+  function changeFontSize() {
+    document.getElementById('myHeader').style.fontSize = "10px";
   }
 }
 
 function draw() {
   // put drawing code here
+  let imgObject = document.getElementById("myImg");
+  imgObject.width = window.innerWidth / 2;
 
+  let textObject = document.getElementById("myText");
+  textObject.width = window.innerWidth / 2;
 }
