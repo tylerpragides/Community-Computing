e1c69872e09a894d8804d764ae79b46ec8f73240
tylerpragides <32971540+tylerpragides@users.noreply.github.com>
2020-01-29 12:41:12 -0800

    k

--- 1-Basic-HTML/sketch.js
+++ 1-Basic-HTML/sketch.js
@@ -1,11 +1,29 @@
 function setup() {
+
+  // var canvas = document.getElementById("myCanvas");
+  // var ctx = canvas.getContext("2d");
+  // ctx.fillStyle = "black";
+  // ctx.fillRect(0,0,canvas.width,canvas.height)
+  //
+  // document.getElementById("myCanvas").addEventListener('click',drawDot);
+  //
+  // function drawDot(){
+  //   ctx.beginPath()
+  //   ctx.strokeStyle = "red";
+  //   var rawX = event.clientX;
+  //   var rawY = event.clientY;
+  //   var fixedX = (rawX - (window.innerWidth - canvas.width) / 2);
+  //   ctx.arc(fixedX,event.clientY,5,0,2 * Math.PI);
+  //   ctx.stroke()
+  // }
+
   document.getElementById('myImg').addEventListener("click",changeImage);
 
   const images = ['chair.jpg', 'html.jpeg', 'websites.jpg', 'table.jp2', 'randomCircle.png'];
   let whichImage = 0;
 
   function changeImage() {
-    document.getElementById('myImg').src = images[whichImage];
+    document.getElementById('myImg').src = images[whichImage % images.length];
     whichImage++;
     if (whichImage >= images.length){
       whichImage = 0;
